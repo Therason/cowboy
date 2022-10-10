7b4e02768da60b0b0474b1fa24e4c0a82a816a19
Thomas Broadwater <tgbroadh2o@gmail.com>
2022-10-10 18:57:20 -0400

    Basic grid layout

--- cmd/cowboy.go
+++ cmd/cowboy.go
@@ -4,11 +4,25 @@ import (
 	"github.com/rivo/tview"
 )
 
+func NewPrimitive(text string) tview.Primitive {
+	return tview.NewTextView().
+					SetTextAlign(tview.AlignCenter).
+					SetText(text)
+}
+
 func Render() {
-	box := tview.NewBox().
-		SetBorder(true).
-		SetTitle("Cowboy")
-	if err := tview.NewApplication().SetRoot(box, true).Run(); err != nil {
+	parent := NewPrimitive("Parent")
+	current := NewPrimitive("Current")
+
+	grid := tview.NewGrid().
+					SetColumns(20, 0).
+					SetRows(0).
+					SetBorders(true).
+					AddItem(parent, 0, 0, 1, 1, 0, 0, false).
+					AddItem(current, 0, 1, 1, 1, 0, 0, false)
+
+	if err := tview.NewApplication().SetRoot(grid, true).SetFocus(grid).Run(); err != nil {
 		panic(err)
 	}
+
 }
